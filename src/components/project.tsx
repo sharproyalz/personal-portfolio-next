d60307de5c5819f153425b9254aa3127884e19e6
sharproyalz <116153953+sharproyalz@users.noreply.github.com>
2024-04-24 13:16:18 +0000

    chore: images and links of project section

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export function ProjectSection() {
   return (
     <>
@@ -12,22 +15,67 @@ export function ProjectSection() {
             <div className="mt-[1.5rem] h-[2px] w-[20%] bg-primary"></div>
 
             <div className="mt-[5rem] flex gap-8">
-              <div className="flex h-[16rem] w-[24rem] flex-col justify-between rounded-2xl bg-gray p-4">
-                <div className="text-[2rem]">Capstone Project</div>
+              <Link
+                href={`https://sds-ss.vercel.app/`}
+                target="_blank"
+                className="group flex w-[24rem] flex-col gap-[1rem] rounded-2xl bg-gray p-4 hover:bg-gray/80"
+              >
+                <div className="text-[2rem] group-hover:text-primary group-hover:underline">
+                  Capstone Project
+                </div>
+
+                <Image
+                  src={`/capstone-project-banner.png`}
+                  alt={`Sharpie Banner`}
+                  height={256}
+                  width={384}
+                  className=""
+                />
+
                 <div className="">
                   Next.js, TypeScript, Tailwind, tRPC, Postgres
                 </div>
-              </div>
+              </Link>
 
-              <div className="flex h-[16rem] w-[24rem] flex-col justify-between rounded-2xl bg-gray p-4">
-                <div className="text-[2rem]">Ecommerce</div>
+              <Link
+                href={`https://sharpie-store.vercel.app/`}
+                target="_blank"
+                className="group flex w-[24rem] flex-col gap-[1rem] rounded-2xl bg-gray p-4 hover:bg-gray/80"
+              >
+                <div className="text-[2rem] group-hover:text-primary group-hover:underline">
+                  Ecommerce
+                </div>
+
+                <Image
+                  src={`/sharpie-banner.png`}
+                  alt={`Sharpie Banner`}
+                  height={256}
+                  width={384}
+                  className=""
+                />
+
                 <div className="">Next.js, TypeScript, Tailwind, Postgres</div>
-              </div>
+              </Link>
 
-              <div className="flex h-[16rem] w-[24rem] flex-col justify-between rounded-2xl bg-gray p-4">
-                <div className="text-[2rem]">Background Changer</div>
+              <Link
+                href={`https://background-changer-one.vercel.app/`}
+                target="_blank"
+                className="group flex w-[24rem] flex-col gap-[1rem] rounded-2xl bg-gray p-4 hover:bg-gray/80"
+              >
+                <div className="text-[2rem] group-hover:text-primary group-hover:underline">
+                  Background Changer
+                </div>
+
+                <Image
+                  src={`/background-changer-banner.png`}
+                  alt={`Sharpie Banner`}
+                  height={256}
+                  width={384}
+                  className=""
+                />
+
                 <div className="">JavaScript</div>
-              </div>
+              </Link>
             </div>
 
             <div className="flex w-full justify-end">
@@ -42,14 +90,79 @@ export function ProjectSection() {
             <div className="mt-[2rem] w-full justify-start">
               <div className="text-[1.5rem]">Technical skills</div>
 
-              <div className="mt-[0.5rem] flex gap-[1.5rem]">
-                <div className="h-[4rem] w-[4rem] bg-gray"></div>
-                <div className="h-[4rem] w-[4rem] bg-gray"></div>
-                <div className="h-[4rem] w-[4rem] bg-gray"></div>
-                <div className="h-[4rem] w-[4rem] bg-gray"></div>
-                <div className="h-[4rem] w-[4rem] bg-gray"></div>
-                <div className="h-[4rem] w-[4rem] bg-gray"></div>
-                <div className="h-[4rem] w-[4rem] bg-gray"></div>
+              <div className="mt-[0.5rem] flex gap-[2rem]">
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/next.png`}
+                    alt={`Next JS Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/tailwind.png`}
+                    alt={`Tailwind CSS Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/prisma.png`}
+                    alt={`Prisma Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/zod.png`}
+                    alt={`Zod Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/postgreSQL.png`}
+                    alt={`PostgreSQL Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/mySQL.png`}
+                    alt={`mySQL Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/javascript.png`}
+                    alt={`JavaScript Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/figma.png`}
+                    alt={`Figma Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
+                <div className="flex h-[4rem] w-[4rem] items-center justify-center">
+                  <Image
+                    src={`/tech-icons/java.png`}
+                    alt={`Java Icon`}
+                    width={48}
+                    height={48}
+                  />
+                </div>
               </div>
             </div>
           </div>
