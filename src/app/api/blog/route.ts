40c226aa73537d7cac30739bccc518d3a7852934
sharproyalz <116153953+sharproyalz@users.noreply.github.com>
2024-05-11 05:35:19 +0000

    Pending changes exported from your codespace

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -12,6 +12,70 @@ export async function POST(req: Request) {
 
     return Response.json(result);
   } catch (err) {
-    return Response.json({ message: "Sending message failed", error: "500" });
+    return Response.json({
+      message: "Creating a new blog failed",
+      error: "500",
+    });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const result = await prisma.blog.findMany();
+
+    if (!result) {
+      return Response.json({
+        message: "Error: Blog not found",
+        status: 404,
+      });
+    }
+
+    return Response.json(result);
+  } catch (error) {
+    console.error("Error fetching blog:", error);
+    return Response.json({
+      message: "Error fetching blog",
+      status: 500,
+    });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id"); 
+    const body = await req.json();
+
+    const updatedBlog = await prisma.blog.update({
+      where: {
+        id: id as string,
+      },
+      data: {
+        ...body,
+      },
+    });
+
+    return Response.json(updatedBlog);
+  } catch (error) {
+    console.error("Error updating blog:", error);
+    return Response.json({ message: "Error updating blog", status: 500 });
+  }
+}
+
+export async function DELETE(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id"); 
+
+    const deletedBlog = await prisma.blog.delete({
+      where: {
+        id: id as string,
+      },
+    });
+
+    return Response.json(deletedBlog);
+  } catch (error) {
+    console.error("Error updating blog:", error);
+    return Response.json({ message: "Error updating blog", status: 500 });
   }
 }
--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -12,6 +12,70 @@ export async function POST(req: Request) {
 
     return Response.json(result);
   } catch (err) {
-    return Response.json({ message: "Sending message failed", error: "500" });
+    return Response.json({
+      message: "Creating a new project failed",
+      error: "500",
+    });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const result = await prisma.project.findMany();
+
+    if (!result) {
+      return Response.json({
+        message: "Error: Project not found",
+        status: 404,
+      });
+    }
+
+    return Response.json(result);
+  } catch (error) {
+    console.error("Error fetching project:", error);
+    return Response.json({
+      message: "Error fetching project",
+      status: 500,
+    });
+  }
+}
+
+export async function PUT(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id"); 
+    const body = await req.json();
+
+    const updatedBlog = await prisma.project.update({
+      where: {
+        id: id as string,
+      },
+      data: {
+        ...body,
+      },
+    });
+
+    return Response.json(updatedBlog);
+  } catch (error) {
+    console.error("Error updating project:", error);
+    return Response.json({ message: "Error updating project", status: 500 });
+  }
+}
+
+export async function DELETE(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id"); 
+
+    const deletedBlog = await prisma.project.delete({
+      where: {
+        id: id as string,
+      },
+    });
+
+    return Response.json(deletedBlog);
+  } catch (error) {
+    console.error("Error updating project:", error);
+    return Response.json({ message: "Error updating project", status: 500 });
   }
 }
