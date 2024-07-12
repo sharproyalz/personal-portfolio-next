f079943b5510f555b2a84fe5ebc259938b569d39
sharproyalz <vopactol@gmail.com>
2024-07-12 19:06:01 +0800

    chore: add blogs animation

--- src/app/_components/blog.tsx
+++ src/app/_components/blog.tsx
@@ -1,8 +1,64 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 import { truncateWord } from "~/utils/truncate-word";
 
 export function BlogSection() {
+  const blogs = [
+    {
+      imageLink: "/article-banner/summary-portfolio.png",
+      imageAlt: "Summary Portfolio Banner",
+      blogTitle: "Summary of My Digital Career: A Personal Portfolio Website",
+      date: "April 2024",
+      tags: ["Improvement", "Next.js"],
+      blogLink: "/blog",
+    },
+    {
+      imageLink: "/article-banner/sharpie-banner.png",
+      imageAlt: "Sharpie Banner",
+      blogTitle: "My First Full-stack Website: Sharpie",
+      date: "March 2024",
+      tags: ["Improvement", "Next.js"],
+      blogLink:
+        "https://www.linkedin.com/posts/vonn-pactol-718a2a283_fullstack-webdeveloper-selfimprovement-activity-7180210384782462976-U5Uw?utm_source=share&utm_medium=member_desktop",
+    },
+    {
+      imageLink: "/article-banner/leetcode-banner.png",
+      imageAlt: "Leetcode Banner",
+      blogTitle:
+        "Enhancing My Coding Skills: My Journey Through 28 Days of LeetCode Problem Solving",
+      date: "February 2024",
+      tags: ["Improvement", "Next.js"],
+      blogLink:
+        "https://www.linkedin.com/feed/update/urn:li:linkedInArticle:7168522598362832896/",
+    },
+  ];
+  const reveal = useRef<Array<HTMLDivElement | null>>([]);
+
+  if (typeof window !== "undefined") {
+    window.addEventListener(
+      "scroll",
+      () => {
+        if (reveal.current) {
+          for (let i = 0; i < blogs.length; i++) {
+            const windowHeight = window.innerHeight;
+            const revealTop = reveal.current[i]?.getBoundingClientRect().top;
+            const revealPoint = 100;
+
+            if (revealTop! < windowHeight - revealPoint) {
+              reveal.current[i]?.classList.add("active");
+            } else {
+              reveal.current[i]?.classList.remove("active");
+            }
+          }
+        }
+      },
+      false,
+    );
+  }
+
   return (
     <>
       <section id="blog" className="mt-[4rem] flex px-4 md:px-12 ">
@@ -19,147 +75,64 @@ export function BlogSection() {
           </div>
 
           <div className="mt-[5rem] flex flex-col items-center justify-between gap-8 lg:flex-row lg:items-stretch">
-            <div className="w-[22rem] rounded-2xl bg-gray p-4 dark:bg-card ">
-              <div className="flex h-[12rem] w-full object-fill lg:w-[20rem]">
-                <Image
-                  src={`/article-banner/summary-portfolio.png`}
-                  alt="Summary Portfolio Banner"
-                  width={320}
-                  height={192}
-                  className="rounded-md"
-                />
-              </div>
-
-              {/* Title for Medium Screen */}
+            {blogs.map((blog, blogIdx) => (
               <div
-                className={`${"Summary of My Digital Career: A Personal Portfolio Website".length > 30 ? "text-xl" : "text-2xl"} mt-[0.5rem] hidden dark:text-gray md:block`}
+                key={blogIdx}
+                ref={(el) => {
+                  if (el && reveal.current) {
+                    reveal.current[blogIdx] = el; // Store reference in the array at the specified index
+                  }
+                }}
+                className="reveal w-[22rem] rounded-2xl bg-gray p-4 dark:bg-card"
               >
-                Summary of My Digital Career: A Personal Portfolio Website
-              </div>
-
-              {/* <div className="mt-[0.5rem] hidden text-[2rem] dark:text-gray md:block">
-                {truncateWord(
-                  "Summary of My Digital Career: A Personal Portfolio Website",
-                )}
-              </div> */}
-
-              {/* Title for Mobile Screen */}
-
-              <div className="my-[1rem]">April 2024</div>
-
-              {/* Tags */}
-              <div className="mb-[0.5rem] flex gap-[0.5rem]">
-                <div className="w-fit rounded-2xl bg-white px-[1rem] py-[0.25rem] text-xs dark:text-black md:text-base">
-                  Improvement
+                <div className="flex h-[12rem] w-full object-fill lg:w-[20rem]">
+                  <Image
+                    src={blog.imageLink}
+                    alt={blog.imageAlt}
+                    width={320}
+                    height={192}
+                    className="rounded-md"
+                  />
                 </div>
-                <div className="w-fit rounded-2xl bg-white px-[1rem] py-[0.25rem] text-xs dark:text-black md:text-base">
-                  Next.js
-                </div>
-              </div>
 
-              <div className="mb-[1rem] h-[2px] bg-white"></div>
-
-              <div className="flex justify-end">
-                <Link
-                  href={`/blog`}
-                  className=" w-fit rounded-2xl bg-secondary px-[1rem] py-[0.25rem] font-bold text-white hover:bg-secondary/90 active:scale-95"
+                {/* Title for Medium Screen */}
+                <div
+                  className={`mt-[0.5rem] hidden h-[64px] overflow-hidden overflow-ellipsis text-2xl dark:text-gray md:block`}
                 >
-                  Read article
-                </Link>
-              </div>
-            </div>
-
-            <div className="w-[22rem] rounded-2xl bg-gray p-4 dark:bg-card">
-              <div className="flex h-[12rem] w-full object-fill lg:w-[20rem]">
-                <Image
-                  src={`/article-banner/sharpie-banner.png`}
-                  alt="Sharpie Banner"
-                  width={320}
-                  height={192}
-                  className="rounded-md"
-                />
-              </div>
-
-              {/* Title for Medium Screen */}
-              <div
-                className={`${"My First Full-stack Website: Sharpie".length > 40 ? "text-xl" : "My First Full-stack Website: Sharpie".length > 25 ? "text-2xl" : "text-3xl"} mt-[0.5rem] hidden dark:text-gray md:block`}
-              >
-                My First Full-stack Website: Sharpie
-              </div>
-
-              <div className="my-[1rem]">March 2024</div>
-
-              {/* Tags */}
-              <div className="mb-[0.5rem] flex gap-[0.5rem]">
-                <div className="w-fit rounded-2xl bg-white px-[1rem] py-[0.25rem] text-xs dark:text-black md:text-base">
-                  Improvement
+                  {blog.blogTitle}
                 </div>
-                <div className="w-fit rounded-2xl bg-white px-[1rem] py-[0.25rem] text-xs dark:text-black md:text-base">
-                  Next.js
+
+                {/* Title for Mobile Screen */}
+                <div className="mt-[0.5rem] block text-[1rem] dark:text-gray md:hidden">
+                  {blog.blogTitle}
                 </div>
-              </div>
+                <div className="my-[1rem]">{blog.date}</div>
 
-              <div className="mb-[1rem] h-[2px] bg-white"></div>
-
-              <div className="flex justify-end">
-                <Link
-                  href={`https://www.linkedin.com/posts/vonn-pactol-718a2a283_fullstack-webdeveloper-selfimprovement-activity-7180210384782462976-U5Uw?utm_source=share&utm_medium=member_desktop`}
-                  className="w-fit rounded-2xl bg-secondary px-[1rem] py-[0.25rem] font-bold text-white hover:bg-secondary/90 active:scale-95"
-                  target="_blank"
-                >
-                  Read article
-                </Link>
-              </div>
-            </div>
-
-            <div className="w-[22rem] rounded-2xl bg-gray p-4 dark:bg-card">
-              <div className="flex h-[12rem] w-full object-fill lg:w-[20rem]">
-                <Image
-                  src={`/article-banner/leetcode-banner.png`}
-                  alt="Leetcode Banner"
-                  width={320}
-                  height={192}
-                  className="rounded-md"
-                />
-              </div>
-
-              {/* Title for Medium Screen */}
-              <div
-                className={`mt-[0.5rem] hidden h-[64px] overflow-hidden overflow-ellipsis text-2xl dark:text-gray md:block`}
-              >
-                Enhancing My Coding Skills: My Journey Through 28 Days of
-                LeetCode Problem Solving
-              </div>
-
-              {/* Title for Mobile Screen */}
-              <div className="mt-[0.5rem] block text-[1rem] dark:text-gray md:hidden">
-                Enhancing My Coding Skills: My Journey Through 28 Days of
-                LeetCode Problem Solving
-              </div>
-              <div className="my-[1rem]">February 2024</div>
-
-              {/* Tags */}
-              <div className="mb-[0.5rem] flex gap-[0.5rem]">
-                <div className="w-fit rounded-2xl bg-white px-[1rem] py-[0.25rem] text-xs dark:text-black md:text-base">
-                  Improvement
+                {/* Tags */}
+                <div className="mb-[0.5rem] flex gap-[0.5rem]">
+                  {blog.tags.map((tag, tagIdx) => (
+                    <div
+                      key={tagIdx}
+                      className="w-fit rounded-2xl bg-white px-[1rem] py-[0.25rem] text-xs dark:text-black md:text-base"
+                    >
+                      {tag}
+                    </div>
+                  ))}
                 </div>
-                <div className="w-fit rounded-2xl bg-white px-[1rem] py-[0.25rem] text-xs dark:text-black md:text-base">
-                  JavaScript
+
+                <div className="mb-[1rem] h-[2px] bg-white"></div>
+
+                <div className="flex justify-end">
+                  <Link
+                    href={blog.blogLink}
+                    className="w-fit rounded-2xl bg-secondary px-[1rem] py-[0.25rem] font-bold text-white hover:bg-secondary/90 active:scale-95"
+                    target="_blank"
+                  >
+                    Read article
+                  </Link>
                 </div>
               </div>
-
-              <div className="mb-[1rem] h-[2px] bg-white"></div>
-
-              <div className="flex justify-end">
-                <Link
-                  href={`https://www.linkedin.com/feed/update/urn:li:linkedInArticle:7168522598362832896/`}
-                  className="w-fit rounded-2xl bg-secondary px-[1rem] py-[0.25rem] font-bold text-white hover:bg-secondary/90 active:scale-95"
-                  target="_blank"
-                >
-                  Read article
-                </Link>
-              </div>
-            </div>
+            ))}
           </div>
 
           <div className="flex justify-end">
